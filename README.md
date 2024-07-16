850e8d1515b4e2de0afb910dbbc1ca2fceaa9b87
pailaghising <papilaghising1111@gmail.com>
2024-07-16 14:29:20 +0545

    added ingress rule https 443

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # laravel_chat_app
+
+
+certbot --nginx -n --agree-tos --redirect --email {{ eamil }} -d {{ domain_name }}
--- terraform/security.tf
+++ terraform/security.tf
@@ -7,6 +7,6 @@ module "security_group" {
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "all-icmp", "mysql-tcp"]
+  ingress_rules       = ["http-80-tcp", "all-icmp", "mysql-tcp" ,"https-443-tcp"]
   egress_rules        = ["all-all"]
 }
