c2dd47e7c13bb8cc77efefb2ff6a1806e3402d7f
pailaghising <papilaghising1111@gmail.com>
2024-07-16 09:07:50 +0545

    sg error resolved for updating existing sg

--- terraform/security.tf
+++ terraform/security.tf
@@ -7,6 +7,6 @@ module "security_group" {
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "mysql-tcp"]
+  ingress_rules       = ["http-80-tcp", "all-icmp", "mysql-tcp"]
   egress_rules        = ["all-all"]
 }
